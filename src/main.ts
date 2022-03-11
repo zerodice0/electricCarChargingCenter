b789a0c399daba643362e0407d46e2553dd55968
zerodice0 <zerodice0@gmail.com>
2022-03-11 16:42:30 +0900

    add kakaoMap test codes.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import App from "./components/app/app";
 import './style.css';
 
 const root = document.querySelector('#app');
+const map = document.querySelector('#kakaoMap');
 const shadowRoot = root?.shadowRoot ?? root?.attachShadow({mode: 'open'});
+
+new window.kakao.maps.Map(map, {
+  center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+  level: 3
+});
 
 shadowRoot && App(shadowRoot);
