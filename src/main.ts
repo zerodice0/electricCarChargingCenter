f628d6e8f2501a71789a6190004ba72dc8bec511
zerodice0 <zerodice0@gmail.com>
2022-03-11 17:30:54 +0900

    - replace kakaoMap code to component.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import App from "./components/app/app";
+import appendKakaoMap from "./components/kakaoMap/kakaoMap";
 import './style.css';
+
+declare global {
+  interface Window {
+    kakao: any
+  }
+}
 
 const root = document.querySelector('#app');
 const map = document.querySelector('#kakaoMap');
 const shadowRoot = root?.shadowRoot ?? root?.attachShadow({mode: 'open'});
 
-new window.kakao.maps.Map(map, {
-  center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-  level: 3
-});
-
 shadowRoot && App(shadowRoot);
+map && appendKakaoMap(map);
