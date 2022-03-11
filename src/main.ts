7997e9d77d5a2c8ab464703e870d0a13fcad78f4
zerodice0 <zerodice0@gmail.com>
2022-03-11 14:05:50 +0900

    - fix: call App with shadowRoot.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
-import './style.css'
+import App from "./components/app/app";
+import './style.css';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const root = document.querySelector('#app');
+const shadowRoot = root?.shadowRoot ?? root?.attachShadow({mode: 'open'});
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+shadowRoot && App(shadowRoot);
